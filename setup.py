120b7412a44c48fcfe09754e97e4212572debe3f
kushagra <nagorikushagra9@gmail.com>
2022-11-08 17:37:35 +0100

    fix: bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+"""Package setup."""
+
+from pathlib import Path
 from setuptools import (setup, find_packages)
 
-from cloud_registry import __version__
+root_dir = Path(__file__).parent.resolve()
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+exec(open(root_dir / "cloud_registry" / "version.py").read())
+
+file_name = root_dir / "README.md"
+with open(file_name, "r") as _file:
+    long_description = _file.read()
 
 setup(
     name='cloud-registry',
-    version=__version__,
+    version=__version__,  # noqa: F821
     author='ELIXIR Cloud & AAI',
     author_email='alexander.kanitz@alumni.ethz.ch',
     description=(
