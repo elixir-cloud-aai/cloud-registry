b7422713f3d0df0aa772bb27bb5e9ddc2ad48868
Aditya Arora <adityarora97@gmail.com>
2023-11-09 22:06:22 +0530

    build: clean up project configuration (#41)
    * move coverage.py and mypy config to pyproject.toml
    
    * remove travis config
    
    * move project metadata from setup.py to pyproject.toml
    
    * add  missing newline
    
    * fix version reading in api docs
    
    * move flake8 config to pyproject and move version to pyproject.toml
    
    * fix version dependency
    
    * fix formatting
    
    * fix dependency error
    
    * fix toml dependency

--- cloud_registry/version.py
+++
@@ -1,3 +0,0 @@
-"""Single source of truth for package version."""
-
-__version__ = "0.2.0"
--- docs/api/conf.py
+++ docs/api/conf.py
@@ -12,6 +12,7 @@
 #
 from pathlib import Path
 import sys
+import toml
 
 from sphinx.ext import apidoc
 
@@ -19,7 +20,9 @@ root_dir = Path.cwd().resolve().parents[1]
 
 sys.path.insert(0, root_dir)
 
-exec(open(root_dir / "cloud_registry" / "version.py").read())
+with open(root_dir / "pyproject.toml", "r") as f:
+    config = toml.load(f)
+    __version__ = config["project"]["version"]
 
 
 # -- Project information -----------------------------------------------------
