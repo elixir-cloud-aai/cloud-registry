56f265bc1c1a813339b6028dba820e1a475f2823
Javed Habib <jh4official@gmail.com>
2024-05-24 18:43:12 +0530

    minor fixes

--- cloud_registry/app.py
+++ cloud_registry/app.py
@@ -1,20 +1,23 @@
 from pathlib import Path
+import logging
 from foca import Foca
 from foca.security.auth import validate_token  # noqa: F401
 
 from cloud_registry.ga4gh.registry.service_info import RegisterServiceInfo
 
+logger = logging.getLogger(__name__)
+
 
 def main():
     config_path = Path(__file__).parent / "config.yaml"
     if not config_path.exists():
+        logger.error(f"Config file not found: {config_path}")
         raise FileNotFoundError(f"Config file not found: {config_path}")
     # create app object
     foca = Foca(
         config_file=config_path,
         custom_config_model="service_models.custom_config.CustomConfig",
     )
-    foca.config_file
     app = foca.create_app()
 
     # register service info
