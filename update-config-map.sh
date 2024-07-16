fa8a1b0dd1361751574550116150a630035dc199
Alvaro Gonzalez <alvaro.gonzalez@csc.fi>
2024-07-16 09:20:57 +0300

    Allow mongodb host config (#52)
    * Fix bug
    
    * Add dependdency

--- update-config-map.sh
+++ update-config-map.sh
@@ -38,6 +38,7 @@ echo "Current Kubernetes namespace: $NAMESPACE"; echo
 echo " * Getting current default configuration"
 
 APP_CONFIG=$(yq --arg HOST_NAME "$HOST_NAME" \
+                --arg MONGO_HOST "$MONGO_HOST" \
     '.endpoints.service.url_prefix = "https" |
      .endpoints.service.external_host = $HOST_NAME |
      .endpoints.service.external_port = 443 |
